963631251ab2a8f7242d2726caa08a5251dd6851
ggrassiant <guillaume.grassiant@gmail.com>
2020-07-25 16:21:42 -0400

    add api redirect

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
 Rails.application.routes.default_url_options[:host] = 'http://localhost:3000'
 
 Rails.application.routes.draw do
+  root to: redirect('/api/v1/items')
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :items, only: %i[index show update create destroy]
