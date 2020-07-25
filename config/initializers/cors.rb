918bb4c1fc32ec96fc310ec95d89211d7d7a0848
ggrassiant <guillaume.grassiant@gmail.com>
2020-07-25 18:44:00 -0400

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -13,6 +13,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
     resource '*',
              headers: :any,
-             methods: %i[get post patch delete head]
+             methods: %i[get post patch put delete head]
   end
 end
