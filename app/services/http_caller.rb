6d893fbe5076035e8626c8799c75b9a7649c67c5
Guillaume Grassiant <guillaume.grassiant@gmail.com>
2021-04-25 16:54:45 -0400

    add test on http caller

--- app/services/http_caller.rb
+++ app/services/http_caller.rb
@@ -1,6 +1,8 @@
 class HttpCaller
   require "http"
 
+  attr_reader :message
+
   def initialize(endpoint:, params: {}, token: '')
     @endpoint = endpoint
     @params = params
@@ -8,17 +10,24 @@ class HttpCaller
   end
 
   def run!
-    JSON.parse(request.body.to_s, object_class: OpenStruct)
-  end
-
-  private
-
-  def request
-    request = HTTP.timeout(5)
+    begin
+      request = HTTP.timeout(5)
       .headers(accept: "application/json")
       .auth(auth)
       .get(@endpoint, params: @params)
+
+    rescue StandardError => e
+      @message = e.to_s
+    end
+    
+    JSON.parse(request.body.to_s, object_class: OpenStruct)
   end
+
+  def successful?
+    message.blank?
+  end
+
+  private
 
   def auth
     "Bearer #{@token}"
